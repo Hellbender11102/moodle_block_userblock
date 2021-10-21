13886144d189d4eb37fbaa3bde2b21caf60ef102
Schindler <schindlerl@th-luebeck.de>
2021-10-21 09:24:28 +0200

    init

---
+++ db/access.php
@@ -0,0 +1,26 @@
+<?php
+$capabilities = array(
+
+    'block/userblock:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/userblock:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
---
+++ lang/en/block_userblock.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * ${userblock} file description here.
+ *
+ * @package    ${userblock}
+ * @copyright  2021 SysBind Ltd. <service@sysbind.co.il>
+ * @auther     schindlerl
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'userblock';
+$string['userblock'] = 'user block';
+$string['userblock:addinstance'] = 'Add a new user block';
+$string['userblock:myaddinstance'] = 'Add a new user block to the My Moodle page';
